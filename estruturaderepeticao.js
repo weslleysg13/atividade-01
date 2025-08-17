506292d6bba6c39b8283cc571a2140a17d77ec4e
Weslley SG <42002498+weslleysg13@users.noreply.github.com>
2025-08-17 20:51:51 -0300

    Alterações no if e no for, foi incrementado quem são as pessoas 1 e 2, além de uma condição dentro do for para saber se a compra é sal ou não, pois foi esquecido de comprar o sal

--- estruturaderepeticao.js
+++ estruturaderepeticao.js
@@ -3,5 +3,9 @@ const listadeCompras = ["arroz","feijao","macarrao","iogurte","acucar","sal"];
 console.log("Fui ao mecardo e comprei:");
 
 for (const compra of listadeCompras){
-    console.log(compra);
+    if(compra === "sal"){
+        console.log("Esqueci de comprar o", compra);
+    } else {
+        console.log(compra);
+    }
 }
--- ifeelse.js
+++ ifeelse.js
@@ -1,14 +1,16 @@
+const pessoa1 = "Weslley"; // Variável para o nome da pessoa 1
 const idade = 28; // Variável para a idade da pessoa 1
+const pessoa2 = "Biel"; // Variável para o nome da pessoa 2
 const idade2 = 12; // Variável para a idade da pessoa 2
 
 if(idade >= 18){ // Condição de teste para verificação se a pessoa 1 é maior ou menor de idade
-    console.log("A pessoa 1 é Adulta(o)!")
+    console.log('O' ,pessoa1, "é Adulto!");
 }else{
-    console.log("A pessoa 1 é menor de idade!")
+    console.log('O' ,pessoa1, "é menor de idade!");
 }
 
 if(idade2 >= 18){ // Condição de teste para verificação se a pessoa 2 é maior ou menor de idade
-    console.log("A pessoa 2 é Adulta(o)!")
+    console.log('O' ,pessoa2, "é Adulto!");
 }else{
-    console.log("A pessoa 2 é menor de idade!")
+    console.log('O' ,pessoa2, "é menor de idade!");
 }
