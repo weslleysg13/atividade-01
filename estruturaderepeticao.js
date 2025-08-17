56550219ac8d576aae1180fe2ce97881b97cb49a
Weslley SG <42002498+weslleysg13@users.noreply.github.com>
2025-08-17 20:25:12 -0300

    Primeiro envio da atividade 01

---
+++ estruturaderepeticao.js
@@ -0,0 +1,7 @@
+const listadeCompras = ["arroz","feijao","macarrao","iogurte","acucar","sal"];
+
+console.log("Fui ao mecardo e comprei:");
+
+for (const compra of listadeCompras){
+    console.log(compra);
+}
---
+++ ifeelse.js
@@ -0,0 +1,14 @@
+const idade = 28; // Variável para a idade da pessoa 1
+const idade2 = 12; // Variável para a idade da pessoa 2
+
+if(idade >= 18){ // Condição de teste para verificação se a pessoa 1 é maior ou menor de idade
+    console.log("A pessoa 1 é Adulta(o)!")
+}else{
+    console.log("A pessoa 1 é menor de idade!")
+}
+
+if(idade2 >= 18){ // Condição de teste para verificação se a pessoa 2 é maior ou menor de idade
+    console.log("A pessoa 2 é Adulta(o)!")
+}else{
+    console.log("A pessoa 2 é menor de idade!")
+}
